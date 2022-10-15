7e6293cb7895388c16dfa14f167d4363d0322dfd
niteenkum <81243993+niteenkum@users.noreply.github.com>
2022-10-15 11:25:03 +0530

    updated

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 
+// For sass middleware
 const sassMiddleware = require('node-sass-middleware');
 
 
