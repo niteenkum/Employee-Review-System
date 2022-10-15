528e8aaec704029b60596b16ba25f8d8188f1d07
niteenkum <81243993+niteenkum@users.noreply.github.com>
2022-10-15 18:07:22 +0530

    updated user controller

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -40,14 +40,14 @@ module.exports.create = function (req, res) {
 
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) {
-      console.log("Error in finding the user in sign up");
-      return;
+      console.log("Error in finding the user in sign up", err);
+      res.redirect("back");;
     }
     if (!user) {
       User.create(req.body, function (err, user) {
         if (err) {
-          console.log("Error in creating the user in sign up");
-          return;
+          console.log("Error in creating the user in sign up", err);
+          res.redirect("back");
         }
         return res.redirect("/users/sign-in");
       });
